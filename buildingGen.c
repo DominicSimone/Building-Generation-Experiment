7be7e54fd56db7d804493e0ed06abe8a8a8c3d0a
DominicSimone <dominic.simone@gmail.com>
2018-04-02 21:38:16 -0700

    Added floor clipping and filling as well as getting rid of subsequent empty top floors

--- buildingGen.c
+++ buildingGen.c
@@ -42,6 +42,73 @@ Floor* generateFloorFromFloor(Floor* prev){
   return floor;
 }
 
+
+//Check the surrounding 8 spaces, helper function
+//Return the number of spaces occupied
+int countNeighbors(Floor* floor, int row, int col){
+  unsigned int numNeighbors = 0;
+
+  if(col > 0){
+    if(floor->map[row][col - 1] != 0){
+      numNeighbors +=1;
+    }
+  }
+
+  if(row > 0){
+    if(floor->map[row - 1][col] != 0){
+      numNeighbors += 1;
+    }
+  }
+
+  if(row < floor->width - 1){
+    if(floor->map[row + 1][col] != 0){
+      numNeighbors += 1;
+    }
+  }
+
+  if(col < floor->height - 1){
+    if(floor->map[row][col + 1] != 0){
+      numNeighbors += 1;
+    }
+  }
+
+  return numNeighbors;
+}
+
+//Cut out rooms that are seperated from others
+//  1 0 1    1 0 1
+//  0 1 0 -> 0 0 0
+//  0 0 0    0 0 0
+void clipFloor(Floor* floor, int adjacentRooms){ 
+  for(int row = 0; row < floor->width; row++){
+    for(int col = 0; col < floor->height; col++){
+      //If space has a room, check surrounding spots
+      if(floor->map[row][col] == 1){
+        if(countNeighbors(floor, row, col) <= adjacentRooms){
+          floor->map[row][col] = 0;
+        }
+      }
+    }
+  }
+}
+
+//Fill in spaces that are surrounded by n rooms (specified by argument)
+//  0 1 0    0 1 0
+//  1 0 1 -> 1 1 1
+//  1 1 0    1 1 0
+void fillFloor(Floor* floor, int adjacentRooms){
+  for(int row = 0; row < floor->width; row++){
+    for(int col = 0; col < floor->height; col++){
+      //If space is empty, check surrounding spots
+      if(floor->map[row][col] == 0){
+        if(countNeighbors(floor, row, col) >= adjacentRooms){
+          floor->map[row][col] = 1;
+        }
+      }
+    }
+  }
+}
+
 Floor* makeFloor(int Width, int Height, int** Map){
   Floor* floor = malloc(sizeof(Floor));
   floor->width = Width;
@@ -69,18 +136,48 @@ void freeFloor(Floor* floor){
   free(floor);
 }
 
+void clipTopFloors(Building* building){
+  for(int i = building->height - 1; i >= 0; i--){
+    int room = 0;
+    for(int row = 0; row < building->floors[i]->width; row++){
+      for(int col = 0; col < building->floors[i]->height; col++){
+        if(building->floors[i]->map[row][col] != 0){
+          room += 1;
+          //To break the nested for loops
+          row = 1000;
+          col = 1000;
+        }
+      }
+    }
+    if(room == 0){
+      freeFloor(building->floors[i]);
+      building->height -= 1;
+    }
+  }
+}
+
 Building* generateBuilding(int width, int height, int levels){
   Floor** floors = malloc(sizeof(Floor*) * levels);
   floors[0] = generateBase(width, height);
+  fillFloor(floors[0], 4);
+  clipFloor(floors[0], 0);
   for(int i = 1; i < levels; i++){
     floors[i] = generateFloorFromFloor(floors[i-1]);
+    fillFloor(floors[i], 4);
+    clipFloor(floors[i], 0);
   } 
 
   Building* building = malloc(sizeof(Building));
   building->height = levels;
   building->floors = floors;
 
+  clipTopFloors(building);
+
   return building;
+}
+
+void addStairs(Building* building){
+
 }
 
 //A more advanced version of the old print building, prints the whole building
@@ -98,6 +195,7 @@ void printBuilding(Building* building){
     }
     printf("\n");
   }
+  printf("\n");
 }
 
 void freeBuilding(Building* building){
@@ -117,11 +215,11 @@ int main(){
   //Initialize random number gen
   srand(time(NULL));
 
-  Building* building = generateBuilding(width, height, levels);
+  Building* building2 = generateBuilding(width, height, levels);
 
-  printBuilding(building);
+  printBuilding(building2);
 
-  freeBuilding(building);
+  freeBuilding(building2);
 
   return 0;
 }
--- buildingGen.h
+++ buildingGen.h
@@ -12,9 +12,15 @@ typedef struct struct_Building{
 Floor* generateBase(int, int);
 Floor* makeFloor(int, int, int**);
 Floor* generateFloorFromFloor(Floor*);
+void clipFloor(Floor*, int);
+void fillFloor(Floor*, int);
 void printFloor(Floor*);
 void freeFloor(Floor*);
+int countNeighbors(Floor*, int, int);
 
 Building* generateBuilding(int, int, int);
+void addStairs(Building*);
 void printBuilding(Building*);
 void freeBuilding(Building*);
+void clipTopFloors(Building*);
+void addStairs(Building*);
